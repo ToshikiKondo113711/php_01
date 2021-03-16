9b33e5162a67b7a28bc62a5d114b218d175fde2e
ToshikiKondo113711 <80142036+ToshikiKondo113711@users.noreply.github.com>
2021-03-16 20:05:05 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+echo `おみくじ`.`<br>`;
+echo `<img src =’img/daikichi.jpg’>`;
+
+// $code = mt_rand(1,5);
+// $code = 1;
+// $daikichiPath = `img/daikichi.jpg`;
+// $chuukichiPath = `img/chuukichi.jpg`;
+// $shoukichiPath = `img/shoukichi.jpg`;
+// $suekichiPath = `img/suekichi.jpg`;
+// $daikyouPath = `img/daikyou.jpg`;
+
+// if(1 === $code){
+    
+//     echo `<img src =’img/daikichi.jpg’>`;
+// }
+
+?>
+
+
+
+
