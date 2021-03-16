2613015409d0dee854c7eb333ac37fd24615259a
ToshikiKondo113711 <80142036+ToshikiKondo113711@users.noreply.github.com>
2021-03-16 21:27:25 +0900

    Update index.php
    コードの修正

--- index.php
+++ index.php
@@ -1,20 +1,51 @@
 <?php
 
-echo `おみくじ`.`<br>`;
-echo `<img src =’img/daikichi.jpg’>`;
+echo 'おみくじ'.'<br>';
 
-// $code = mt_rand(1,5);
-// $code = 1;
-// $daikichiPath = `img/daikichi.jpg`;
-// $chuukichiPath = `img/chuukichi.jpg`;
-// $shoukichiPath = `img/shoukichi.jpg`;
-// $suekichiPath = `img/suekichi.jpg`;
-// $daikyouPath = `img/daikyou.jpg`;
+$code = mt_rand(1,5);
 
-// if(1 === $code){
+// $daikichiPath = 'img/daikichi.jpg';
+// $chuukichiPath = 'img/chuukichi.jpg';
+// $shoukichiPath = 'img/shoukichi.jpg';
+// $suekichiPath = 'img/suekichi.jpg';
+// $daikyouPath = 'img/daikyou.jpg';
+
+if(1 === $code){
     
-//     echo `<img src =’img/daikichi.jpg’>`;
-// }
+    echo '<img src =img/daikichi.jpg>'.'<br>';
+    echo '大吉'.'<br>';
+    echo $code;
+}
+
+if(2 === $code){
+    
+    echo '<img src =img/chuukichi.jpg>'.'<br>';
+    echo '中吉'.'<br>';
+    echo $code;
+}
+if(3 === $code){
+    
+    echo '<img src =img/shoukichi.jpg>'.'<br>';
+    echo '小吉'.'<br>';
+    echo $code;
+}
+if(4 === $code){
+    
+    echo '<img src =img/suekichi.jpg>'.'<br>';
+    echo '末吉'.'<br>';
+    echo $code;
+}
+if(5 === $code){
+    
+    echo '<img src =img/daikyou.jpg>'.'<br>';
+    echo `大凶`.'<br>';
+    echo $code;
+}
+
+echo '<br>';
+
+
+
 
 ?>
 
