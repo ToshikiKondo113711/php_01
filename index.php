8aa574e1b9d5b5476cc8cd630d059fb5b4d5169a
ToshikiKondo113711 <80142036+ToshikiKondo113711@users.noreply.github.com>
2021-03-17 22:06:19 +0900

    ファイルの修正
    名前と年齢の入力フォームを追加のため修正
    omikuji_post.phpへ入力後→index.phpでおみくじの結果表示される

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 
-echo 'おみくじ'.'<br>';
+
+function h ($val){
+    return htmlspecialchars($val,ENT_QUOTES);
+}
+
+session_start();
+$age = $_POST['age'];
+$name = $_POST['name'];
+
+echo 'おみくじ結果'.'<br>';
+echo H($name).'さん'.'<br>';
+echo  h($age).'歳'.'<br>';
+
 
 $code = mt_rand(1,5);
 
@@ -8,32 +20,32 @@ if(1 === $code){
     
     echo '<img src =img/daikichi.jpg>'.'<br>';
     echo '大吉'.'<br>';
-    echo $code;
+    echo 'ランダムな数字：'.$code;
 }
 
 if(2 === $code){
     
     echo '<img src =img/chuukichi.jpg>'.'<br>';
     echo '中吉'.'<br>';
-    echo $code;
+    echo 'ランダムな数字：'.$code;
 }
 if(3 === $code){
     
     echo '<img src =img/shoukichi.jpg>'.'<br>';
     echo '小吉'.'<br>';
-    echo $code;
+    echo 'ランダムな数字：'.$code;
 }
 if(4 === $code){
     
     echo '<img src =img/suekichi.jpg>'.'<br>';
     echo '末吉'.'<br>';
-    echo $code;
+    echo 'ランダムな数字：'.$code;
 }
 if(5 === $code){
     
     echo '<img src =img/daikyou.jpg>'.'<br>';
     echo `大凶`.'<br>';
-    echo $code;
+    echo 'ランダムな数字：'.$code;
 }
 
 echo '<br>';
@@ -42,6 +54,7 @@ echo '<br>';
 
 
 ?>
+
 
 
 
